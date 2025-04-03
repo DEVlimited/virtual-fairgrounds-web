d424fbc98c5438c90b90b926d9aebaf4adc53b84
bobbyreed <37113385+bobbyreed@users.noreply.github.com>
2025-04-03 15:49:13 -0500

    create landing page

---
+++ app.js
@@ -0,0 +1,11 @@
+const bobby = {
+    "name": "bobby",
+    "status": "professor",
+    "power-level": 9000
+}
+
+const tashfeen = {
+    "name": "Ahmad",
+    "status": "professor",
+    "power-level": 8999
+}
