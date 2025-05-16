d3613410fc4b48b7902cb9288337168c643d9774
xDarthx <76278427+xDarthx@users.noreply.github.com>
2025-05-16 16:36:33 -0500

    Added skybox and a cool fog test setup

--- public/js/cameraTester.js
+++ public/js/cameraTester.js
@@ -4,6 +4,125 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 import { update } from 'three/examples/jsm/libs/tween.module.js';
 
+const _NOISE_GLSL = `
+//
+// Description : Array and textureless GLSL 2D/3D/4D simplex
+//               noise functions.
+//      Author : Ian McEwan, Ashima Arts.
+//  Maintainer : stegu
+//     Lastmod : 20201014 (stegu)
+//     License : Copyright (C) 2011 Ashima Arts. All rights reserved.
+//               Distributed under the MIT License. See LICENSE file.
+//               https://github.com/ashima/webgl-noise
+//               https://github.com/stegu/webgl-noise
+//
+
+vec3 mod289(vec3 x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec4 mod289(vec4 x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec4 permute(vec4 x) {
+     return mod289(((x*34.0)+1.0)*x);
+}
+
+vec4 taylorInvSqrt(vec4 r)
+{
+  return 1.79284291400159 - 0.85373472095314 * r;
+}
+
+float snoise(vec3 v)
+{
+  const vec2  C = vec2(1.0/6.0, 1.0/3.0) ;
+  const vec4  D = vec4(0.0, 0.5, 1.0, 2.0);
+
+// First corner
+  vec3 i  = floor(v + dot(v, C.yyy) );
+  vec3 x0 =   v - i + dot(i, C.xxx) ;
+
+// Other corners
+  vec3 g = step(x0.yzx, x0.xyz);
+  vec3 l = 1.0 - g;
+  vec3 i1 = min( g.xyz, l.zxy );
+  vec3 i2 = max( g.xyz, l.zxy );
+
+  //   x0 = x0 - 0.0 + 0.0 * C.xxx;
+  //   x1 = x0 - i1  + 1.0 * C.xxx;
+  //   x2 = x0 - i2  + 2.0 * C.xxx;
+  //   x3 = x0 - 1.0 + 3.0 * C.xxx;
+  vec3 x1 = x0 - i1 + C.xxx;
+  vec3 x2 = x0 - i2 + C.yyy; // 2.0*C.x = 1/3 = C.y
+  vec3 x3 = x0 - D.yyy;      // -1.0+3.0*C.x = -0.5 = -D.y
+
+// Permutations
+  i = mod289(i);
+  vec4 p = permute( permute( permute(
+             i.z + vec4(0.0, i1.z, i2.z, 1.0 ))
+           + i.y + vec4(0.0, i1.y, i2.y, 1.0 ))
+           + i.x + vec4(0.0, i1.x, i2.x, 1.0 ));
+
+// Gradients: 7x7 points over a square, mapped onto an octahedron.
+// The ring size 17*17 = 289 is close to a multiple of 49 (49*6 = 294)
+  float n_ = 0.142857142857; // 1.0/7.0
+  vec3  ns = n_ * D.wyz - D.xzx;
+
+  vec4 j = p - 49.0 * floor(p * ns.z * ns.z);  //  mod(p,7*7)
+
+  vec4 x_ = floor(j * ns.z);
+  vec4 y_ = floor(j - 7.0 * x_ );    // mod(j,N)
+
+  vec4 x = x_ *ns.x + ns.yyyy;
+  vec4 y = y_ *ns.x + ns.yyyy;
+  vec4 h = 1.0 - abs(x) - abs(y);
+
+  vec4 b0 = vec4( x.xy, y.xy );
+  vec4 b1 = vec4( x.zw, y.zw );
+
+  //vec4 s0 = vec4(lessThan(b0,0.0))*2.0 - 1.0;
+  //vec4 s1 = vec4(lessThan(b1,0.0))*2.0 - 1.0;
+  vec4 s0 = floor(b0)*2.0 + 1.0;
+  vec4 s1 = floor(b1)*2.0 + 1.0;
+  vec4 sh = -step(h, vec4(0.0));
+
+  vec4 a0 = b0.xzyw + s0.xzyw*sh.xxyy ;
+  vec4 a1 = b1.xzyw + s1.xzyw*sh.zzww ;
+
+  vec3 p0 = vec3(a0.xy,h.x);
+  vec3 p1 = vec3(a0.zw,h.y);
+  vec3 p2 = vec3(a1.xy,h.z);
+  vec3 p3 = vec3(a1.zw,h.w);
+
+//Normalise gradients
+  vec4 norm = taylorInvSqrt(vec4(dot(p0,p0), dot(p1,p1), dot(p2, p2), dot(p3,p3)));
+  p0 *= norm.x;
+  p1 *= norm.y;
+  p2 *= norm.z;
+  p3 *= norm.w;
+
+// Mix final noise value
+  vec4 m = max(0.5 - vec4(dot(x0,x0), dot(x1,x1), dot(x2,x2), dot(x3,x3)), 0.0);
+  m = m * m;
+  return 105.0 * dot( m*m, vec4( dot(p0,x0), dot(p1,x1),
+                                dot(p2,x2), dot(p3,x3) ) );
+}
+
+float FBM(vec3 p) {
+  float value = 0.0;
+  float amplitude = 0.5;
+  float frequency = 0.0;
+  for (int i = 0; i < 6; ++i) {
+    value += amplitude * snoise(p);
+    p *= 2.0;
+    amplitude *= 0.5;
+  }
+  return value;
+}
+`;
+
+
 //I found this code in the documentation for loading a GLTF File and it helped me a lot with getting 
 //it to properly load and find the model
 function main() {
@@ -30,16 +149,60 @@ function main() {
 
 	const fov = 55;
 	const aspect = 2; // the canvas default
-	const near = 0.1;
-	const far = 80;
+	const near = 0.9;
+	const far = 650;
 	const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
-	camera.position.set( -53.35, 31.54, 4.64);
+	camera.position.set( -53.35, 34.54, 4.64);
 
     //currently the best Camera Position: X: -53.35, Y: 31.54, Z: 4.64
 
     //This is to help create the split view of the main camera
     //Found the code and full documentation on it in the docs for three.js in Cameras section
     const cameraHelper = new THREE.CameraHelper(camera);
+
+    THREE.ShaderChunk.fog_fragment = `
+    #ifdef USE_FOG
+        vec3 fogOrigin = cameraPosition;
+        vec3 fogDirection = normalize(vWorldPosition - fogOrigin);
+        float fogDepth = distance(vWorldPosition, fogOrigin);
+
+        // f(p) = fbm( p + fbm( p ) )
+        vec3 noiseSampleCoord = vWorldPosition * 0.00025 + vec3(
+            0.0, 0.0, fogTime * 0.025);
+        float noiseSample = FBM(noiseSampleCoord + FBM(noiseSampleCoord)) * 0.5 + 0.5;
+        fogDepth *= mix(noiseSample, 1.0, saturate((fogDepth - 5000.0) / 5000.0));
+        fogDepth *= fogDepth;
+
+        float heightFactor = 0.05;
+        float fogFactor = heightFactor * exp(-fogOrigin.y * fogDensity) * (
+            1.0 - exp(-fogDepth * fogDirection.y * fogDensity)) / fogDirection.y;
+        fogFactor = saturate(fogFactor);
+
+        gl_FragColor.rgb = mix( gl_FragColor.rgb, fogColor, fogFactor );
+    #endif`;
+    
+    THREE.ShaderChunk.fog_pars_fragment = _NOISE_GLSL + `
+    #ifdef USE_FOG
+        uniform float fogTime;
+        uniform vec3 fogColor;
+        varying vec3 vWorldPosition;
+        #ifdef FOG_EXP2
+            uniform float fogDensity;
+        #else
+            uniform float fogNear;
+            uniform float fogFar;
+        #endif
+    #endif`;
+    
+    THREE.ShaderChunk.fog_vertex = `
+    #ifdef USE_FOG
+        vWorldPosition = worldPosition.xyz;
+    #endif`;
+    
+    THREE.ShaderChunk.fog_pars_vertex = `
+    #ifdef USE_FOG
+        varying vec3 vWorldPosition;
+    #endif`;
 
 
     //I found this in the cameras documentation to help me figure out how
@@ -81,25 +244,25 @@ function main() {
 		}
 	}
 
-    class FogGUIHelper {
-        constructor(fog) {
-            this.fog = fog;
-        }
-        get near() {
-            return this.fog.near;
-        }
-        set near(v) {
-            this.fog.near = v;
-            this.fog.far = Math.max(this.fog.far, v);
-        }
-        get far() {
-            return this.fog.far;
-        }
-        set far(v) {
-            this.fog.far = v;
-            this.fog.near = Math.min(this.fog.near, v);
-        }
-    }
+    // class FogGUIHelper {
+    //     constructor(fog) {
+    //         this.fog = fog;
+    //     }
+    //     get near() {
+    //         return this.fog.near;
+    //     }
+    //     set near(v) {
+    //         this.fog.near = v;
+    //         this.fog.far = Math.max(this.fog.far, v);
+    //     }
+    //     get far() {
+    //         return this.fog.far;
+    //     }
+    //     set far(v) {
+    //         this.fog.far = v;
+    //         this.fog.near = Math.min(this.fog.near, v);
+    //     }
+    // }
 
     const gui = new GUI();
     gui.add(camera, 'fov', 1, 180);
@@ -127,7 +290,7 @@ function main() {
         60,  // fov
         2,   // aspect
         0.1, // near
-        500, // far
+        1000, // far
     );
     camera2.position.set(0, 100, 100);
     camera2.lookAt(0, 0, 0);
@@ -185,6 +348,7 @@ function main() {
 		} );
 		const mesh = new THREE.Mesh( planeGeo, planeMat );
 		mesh.rotation.x = Math.PI * - .5;
+
 		scene.add( mesh );
 
 	}
@@ -200,14 +364,14 @@ function main() {
 	}
 
     {
-        const near = 0.1;
-        const far = 80;
-        const fogColor = 0x000040;
-        scene.fog = new THREE.Fog(fogColor, near, far);
 
-        const fogGUIHelper = new FogGUIHelper(scene.fog);
-        gui.add(fogGUIHelper, 'near', near, far * 4).listen();
-        gui.add(fogGUIHelper, 'far', near, far * 4).listen();
+        const depth = 0.0000005;
+        const fogColor = 0xDFE9F3;
+        scene.fog = new THREE.FogExp2(fogColor, depth);
+
+        // const fogGUIHelper = new FogGUIHelper(scene.fog);
+        // gui.add(fogGUIHelper, 'depth', depth, 1 ).listen();
+
     }
 
 	{
@@ -223,6 +387,31 @@ function main() {
 		gui.add( light, 'intensity', 0, 5, 0.01 );
 
 	}
+
+    {
+
+        //This is the skybox code, it uses the Equirectangular mapping because it is a panorama image. 
+        //I found that the sphere is better for the project seeing as we are not wanting them to travel too far.
+        let loader = new THREE.TextureLoader();
+        const imagePath = '../public/skybox/Panorama_Sky_23-512x512.png'
+        loader.load( imagePath, (panorameTexture) => {
+
+                const skySphereGeometry = new THREE.SphereGeometry(500, 60, 60);
+
+                panorameTexture.mapping = THREE.EquirectangularReflectionMapping;
+                panorameTexture.colorSpace = THREE.SRGBColorSpace;
+                let skySphereMaterial = new THREE.MeshBasicMaterial({
+                    map: panorameTexture,
+                    fog: false
+                });
+
+                skySphereMaterial.side = THREE.BackSide;
+                let skySphereMesh = new THREE.Mesh(skySphereGeometry, skySphereMaterial);
+
+                scene.add(skySphereMesh);
+            }
+        );
+    }
 
 	{
 
@@ -289,7 +478,7 @@ function main() {
             // don't draw the camera helper in the original view
             cameraHelper.visible = false;
         
-            scene.background.set(0x000040);
+            // scene.background.set(0x000040);
         
             // render
             renderer.render(scene, camera);
@@ -306,7 +495,7 @@ function main() {
             // draw the camera helper in the 2nd view
             cameraHelper.visible = true;
 
-            scene.background.set(0x000040);
+            // scene.background.set(0x000040);
         
             renderer.render(scene, camera2);
         }
@@ -318,26 +507,30 @@ function main() {
 
 	requestAnimationFrame( render );
 
-    window.addEventListener('keydown', function(event) {
+    {
+
+        view1Elem.addEventListener('keydown', function(event) {
         if(event.code == 'KeyW'){
             camera.translateZ(5) ;
         }
-    });
-    window.addEventListener('keydown', function(event) {
-        if(event.code == 'KeyA'){
-            camera.translateX(5);
-        }
-    });
-    window.addEventListener('keydown', function(event) {
-        if(event.code == 'KeyS'){
-            camera.translateZ(-5);
-        }
-    });
-    window.addEventListener('keydown', function(event) {
-        if(event.code == 'KeyD'){
-            camera.translateX(-5);
-        }
-    });
+        });
+        view1Elem.addEventListener('keydown', function(event) {
+            if(event.code == 'KeyA'){
+                camera.translateX(5);
+            }
+        });
+        view1Elem.addEventListener('keydown', function(event) {
+            if(event.code == 'KeyS'){
+                camera.translateZ(-5);
+            }
+        });
+        view1Elem.addEventListener('keydown', function(event) {
+            if(event.code == 'KeyD'){
+                camera.translateX(-5);
+            }
+        });
+
+    }
 
 }
 
